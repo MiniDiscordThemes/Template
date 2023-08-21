51f374676b25f80b1948f34400a14f8ed05be56f
Saltssaumure <themes@saltssaumure.net>
2023-08-21 04:35:25 +0100

    Update all

--- scss-compile.config.js
+++ scss-compile.config.js
@@ -1,13 +1,9 @@
 /** @type {import('bd-scss/lib/config').Config} */
 export default {
     meta: {
-        /** The official name of your theme. */
-        name: "Template",
-        /** The name of your base scss file. */
-        scss: "temp",
-        /** The name of your theme repository. */
-        repo: "Template",
-        /** The version of your theme. */
-        version: "1.0.0"
+        name: "Template" /* The official name of your theme. */,
+        scss: "temp" /* The name of your base scss file. */,
+        repo: "Template" /* The name of your theme repository. */,
+        version: "dev" /* The version of your theme. */
     }
 };
